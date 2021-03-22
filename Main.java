c2b77ca53313f6b64daa68a240e3223a2fa5a0b5
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-03-22 21:46:49 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,53 @@
 package sample.Calculette;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * --------------------------------
+ * PORTABLE CALCULATOR V1.0
+ * Basic functions supported
+ * @author sofoste
+ * --------------------------------
+ */
+public class Main extends Application {
+
+    @FXML
+    private Button exitBtn;
+//    @FXML private Button exitBtn;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setTitle("PORTABLE CALCULATOR V1.0 [@sofoste]");
+
+        primaryStage.setScene(new
+                        Scene(root,
+                        550,
+                        600
+                )
+        );
+
+/*        String decoration = this.getClass().getResource("Decoration.css").toExternalForm();
+        root.getStylesheets().add(decoration);
+*/
+
+
+
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
